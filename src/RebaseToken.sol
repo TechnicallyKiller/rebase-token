9486b6c597025235fe3869ec056fb8f14d0cc03e
Divyanshh Kalra <divyanshhkalra1234@gmail.com>
2025-01-24 22:30:38 +0530

    editing

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -5,6 +5,8 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  contract RebaseToken is ERC20{
     uint256 public s_interestRate= 5e10;
     mapping (address => uint) public _balances;
+    mapping (address => uint256) public userInterestRate;
+    mapping (address => uint256) private s_lastUpdatedTimestamp;
     event interestRate_set(uint256 interestrate);
     error RebaseToken_InterestRateDecrease(uint256 intialInterest, uint256 FinalInterest);
 
@@ -37,14 +39,25 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
         emit interestRate_set(_newInterestRate);
     
    }
+    function mint(address _to,uint256 _amount) external{
+        _mintaccuredInterest(_to);
+    userInterestRate[_to]=s_interestRate;
+        _mint(_to,_amount);
+    }
     function _mintaccuredInterest(address _user) internal view{
+        // (1) find their current balance of rebase tokens that have been minted to the user
+        // (2) calculate their current balance including any interest ->balanceOf
+        // calculate the number of tokens that need to be minted to the user
 
     }
 
-     function balanceOf(address user) public view virtual returns (uint256) {
-        return _balances[account];
+     function balanceOf(address user) public view override virtual returns (uint256) {
+        return _balances[user];
 
-}
+    }
+    function getInterestRate(address _user) external view returns (uint256) {
+        return userInterestRate[_user];
+    }
 
 
  }
